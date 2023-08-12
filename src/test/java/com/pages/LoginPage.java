c6b17a8e5d090d20cd998ddb89598dee8d1babe9
Kafle <kaflepk@amazon.com>
2023-08-12 14:41:48 -0400

    login page and step definitio

--- src/test/java/com/pages/LoginPage.java
+++ src/test/java/com/pages/LoginPage.java
@@ -1,5 +1,32 @@
 package com.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	
+	private WebDriver driver;
+	//By Loctors:
+	
+	private By emailId=By.id("email");
+	private By password=By.id("password");
+	private By signInButton=By.id("Submitlogin");
+	private By forgotPaswordLInks=By.linkText("Forgot your password");
+	//2. Costructor of the page class
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	//3. page action: features(behavior) of the page the form of method:
+	
+	public String getLoginPageTitle() {
+		
+		return driver.getTitle();
+		
+	}
+	public void isForgotPWdLinkExist() {
+		driver.findElement(forgotPaswordLInks);
+	}
+	
+	
 
 }
